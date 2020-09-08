c0e96fb773cf1b72b4d9a4f82e247ad931fc3291
Mariano Heller <marianofheller@gmail.com>
2020-09-07 21:26:38 -0300

    morra

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 
 import Control.Monad.IO.Class
-import Control.Monad.Trans.State.Lazy (StateT, get, runStateT)
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State.Lazy (StateT, get, modify', runStateT)
+import qualified Data.Bifunctor as Bi
 import System.Random (Random (randomRIO))
+
+data Player = Human | AI
 
 type Score = (Integer, Integer)
 
@@ -12,8 +16,8 @@ type GameState = (Score, IO Play)
 
 aiPlay :: IO Play
 aiPlay = do
-  hand <- randomRIO (1, 5)
-  bet <- randomRIO (2, 10)
+  hand <- randomRIO (0, 5)
+  bet <- randomRIO (0, 10)
   return (hand, bet)
 
 playerPlay :: IO Play
@@ -23,31 +27,67 @@ playerPlay = do
   putStrLn "Choose expected score"
   playerBet <- liftIO getUserInput
   return (playerHand, playerBet)
-
-reportPlay :: Play -> Play -> IO ()
-reportPlay (handAI, betAI) (handPlayer, betPlayer) = do
-  putStrLn "+++++++++"
-  putStrLn $ "Computer bet is " ++ (show betAI)
-  putStrLn $ "Computer hand is " ++ (show handAI)
-  putStrLn "+++++++++"
-  putStrLn $ "Player bet is " ++ (show betPlayer)
-  putStrLn $ "Player hand is " ++ (show handPlayer)
-  putStrLn "+++++++++"
-  putStrLn $ "Full play value is " ++ (show $ handAI + handPlayer)
-
-getUserInput :: IO Integer
-getUserInput = fmap parseInput getLine
   where
+    getUserInput = fmap parseInput getLine
     parseInput a = read a :: Integer
 
-app :: StateT GameState IO GameState
-app = do
+reportPlay :: Play -> Play -> StateT GameState IO ()
+reportPlay (handPlayer, betPlayer) (handAI, betAI) = do
+  let print = liftIO . putStrLn
+  let hr = print "+++++++++"
+  hr
+  print $ "Computer bet is " ++ (show betAI)
+  print $ "Computer hand is " ++ (show handAI)
+  hr
+  print $ "Player bet is " ++ (show betPlayer)
+  print $ "Player hand is " ++ (show handPlayer)
+  hr
+  print $ "Full round value is " ++ (show $ handAI + handPlayer)
+  (score, _) <- get
+  print $ "Score value is " ++ (show score)
+
+updateScore :: Play -> Play -> StateT GameState IO ()
+updateScore (handPlayer, betPlayer) (handAI, betAI) = do
+  let addPoint bet = (+)  (if bet == (handAI + handPlayer) then 1 else 0)
+  let _updateScore = Bi.first $ \(ai, player) -> (addPoint betPlayer player, addPoint betAI ai)
+  modify' _updateScore
+
+runRound :: StateT GameState IO ()
+runRound = do
   liftIO $ putStrLn "=========================="
   (score, ai) <- get
   computerChoice <- liftIO ai
   playerChoice <- liftIO playerPlay
-  liftIO $ reportPlay computerChoice playerChoice
-  get
+  updateScore playerChoice computerChoice
+  reportPlay playerChoice computerChoice
+  return ()
+
+evalGame :: StateT GameState IO (Maybe Player)
+evalGame = do
+  (score, _) <- get
+  let f = (flip (>=) $ 3)
+  lift $ case Bi.bimap f f score of
+    (True, _) -> return $ Just Human
+    (_, True) -> return $ Just AI
+    _ -> return Nothing
+
+reportGame :: Player -> StateT GameState IO ()
+reportGame player = do
+  let playerToText p =
+        case p of
+          Human -> "Human"
+          AI -> "Computer"
+  let winner = playerToText player
+  (score, _) <- get
+  liftIO $ putStrLn $ "The winner was " ++ winner ++ "with score: " ++ (show score)
+
+app :: StateT GameState IO ()
+app = do
+  runRound
+  maybeWinner <- evalGame
+  case maybeWinner of
+    Just player -> reportGame player
+    Nothing -> app
 
 main :: IO ()
 main = do
